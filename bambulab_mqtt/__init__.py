43c26fae73cf0b3f17503770f5b95c8db72e1672
Williamnumismatics <169579672+Williamnumismatics@users.noreply.github.com>
2026-02-27 06:41:44 +1100

    added fujnction basedd on my dashboard project but it doesnt work so far

--- bambulab_mqtt/__init__.py
+++ bambulab_mqtt/__init__.py
@@ -1,2 +1,5 @@
+from .client import BambulabPrinter
+
+
 def test():
     print("This is a test function for the bambulab_mqtt package.")
---
+++ bambulab_mqtt/client.py
@@ -0,0 +1,77 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+import random
+import string
+import ssl
+
+
+class BambulabPrinter:
+    def __init__(self, ip, access_code, username="bblp"):
+        self.client = None
+        self.connected = False
+
+        self.ip = ip
+        # self.serial = serial
+        self.access_code = access_code
+        self.username = username
+
+        self.latest_status = {}
+        self.latest_snapshot = None
+        
+    def _on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            topic = f"device/{self.serial}/status"
+            client.subscribe(topic)
+            self.connected = True
+        else:
+            print(f"Connection failed with code {rc}")
+
+    def _on_message(self, client, userdata, msg):
+        try:
+            payload = json.loads(msg.payload.decode())
+            print("Raw data:", json.dumps(payload, indent=2))
+
+            for key, value in payload.items():
+                if key not in self.latest_status:
+                    self.latest_status[key] = value
+                else:
+                    if ininstance(value, dict):
+                        self.latest_status[key].update(value)
+                    else:
+                        self.latest_status[key] = value
+
+        except Exception as e:
+            print(f"Error processing message: {e}")
+
+    def connect(self):
+        client_id = "PythonLib_" + ''.join(random.choices(string.ascii_letters, k=6))
+        self.client = mqtt.Client(client_id=client_id, clean_session=True)
+
+        self.client.username_pw_set(self.username, self.access_code)
+        self.client.tls_set(cert_reqs=ssl.CERT_NONE)
+        self.client.tls_insecure_set(True)
+
+        self.client.on_connect = self._on_connect
+        self.client.on_message = self._on_message
+
+        try:
+            self.client.connect(self.ip, 8883, 60)
+            self.client.loop_start()
+        except Exception as e:
+            print(f"Connection error: {e}")
+            return False
+        
+    def request_full_status(self):
+        if not self.connected:
+            print("Not connected to printer")
+            return None
+        
+        payload = {
+            "pushing": {
+                "sequence_id" : 1,
+                "command" : "pushall",
+                "version" : 1,
+                "push_target" :1
+            }
+        }
