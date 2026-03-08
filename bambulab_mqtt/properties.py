fe81254c86d1a1dd899e045b5ddc6d7968701a13
Williamnumismatics <williamnumismatics@gmail.com>
2026-03-08 14:35:28 +1100

    need to work on pc

--- bambulab_mqtt/properties.py
+++ bambulab_mqtt/properties.py
@@ -13,3 +13,27 @@ class PropertiesMixin:
     @property
     def bed_temp(self):
         return self._print_status().get("bed_temper")
+    
+    @property
+    def wifi_signal(self):
+        return self._print_status().get("wifi_signal")
+    
+    @property
+    def ams_chip_id(self):
+        return self._print_status().get("chip_id")
+    
+    @property
+    def amd_id(self):
+        return self._print_status().get("amd_id")
+    
+    @property
+    def ams_humidity(self):
+        return self._print_status().get("humidity_raw")
+    
+    @property
+    def ams_temp(self):
+        return self._print_status().get("temp")
+    
+    @property
+    def ams_1_tray_type(self):
+        return self._print_status().get("tray_1_type")
